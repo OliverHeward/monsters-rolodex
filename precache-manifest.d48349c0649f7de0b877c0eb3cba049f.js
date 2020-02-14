3b34c099ba5f590f56e44113d82579c90c61fb38
Oliver Heward <oliverhewardbie@gmail.com>
2020-02-14 12:51:41 +0000

    Updates

---
+++ precache-manifest.d48349c0649f7de0b877c0eb3cba049f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2fc34d6e991e7b304969dc59cf3a82",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "54a6e0a8740568b3f1d3",
+    "url": "/monsters-rolodex/static/css/main.6ee34b60.chunk.css"
+  },
+  {
+    "revision": "b59f44af4d4a1bf76f80",
+    "url": "/monsters-rolodex/static/js/2.215fb3a6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.215fb3a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54a6e0a8740568b3f1d3",
+    "url": "/monsters-rolodex/static/js/main.c29e8cb7.chunk.js"
+  },
+  {
+    "revision": "6da5879867585b6861df",
+    "url": "/monsters-rolodex/static/js/runtime-main.cdf3e700.js"
+  }
+]);
