1c91a142a493fdac9084ae823c1612cf527bf2b4
Oliver Heward <oliverhewardbie@gmail.com>
2020-02-14 12:51:13 +0000

    repo push

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+import {CardList} from './Components/CardList/CardList';
+import {SearchBox} from './Components/SearchBox/SearchBox';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  };
+
+  // Arrow function automatically binds (this) for us.
+  // (lexical scoping)
+  handleChange = e => {
+    this.setState({ searchField: e.target.value})
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }))
+  }
+
+  render() {
+    // Array destructuring
+    const { monsters, searchField } = this.state;
+    const filteredMonster = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox 
+          placeholder="search monsters"
+          handleChange={this.handleChange} />
+        <CardList monsters={filteredMonster} />
+      </div>
+    );
+  };
+};
+
+export default App;
